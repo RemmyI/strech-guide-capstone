b0014fc7806211336b693463502c61f554d63baa
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-11-28 21:37:34 -0800

    believe user model is finish, making exercise model,views and urls

--- StrechApp/models.py
+++ StrechApp/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+from users_app.models import CustomUser
+
+class exercises(models.Model):
+    Date = models.DateTimeField(auto_now_add=True)
+    Bodypart = models.CharField(max_length=50)
+    Duration = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f'{self.Bodypart} - {self.Date} - {self.Duration}'
+
+    class Meta:
+        ordering = ['-date']
+
+    def get_absolute_url(self):
+        return reverse('posts:home')
