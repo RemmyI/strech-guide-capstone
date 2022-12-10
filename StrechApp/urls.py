00da45070bce93acdf363478e4c99c604840d2a9
ignacio(iggy) <nachoramirezlopez@gmail.com>
2022-12-09 21:42:40 -0800

    graph displaying. styling in the workds

--- StrechApp/urls.py
+++ StrechApp/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 app_name = 'stretches'
 urlpatterns = [
-    # path('', views.Home.as_view(), name='home'),
-    path('', views.stretches, name='home')
+path('', views.stretches, name='home'),
+path('edit/<int:pk/',views.edit.as_view(), name='edit'),
+path('add/',views.add.as_view(), name='add'),
 ]
--- StrechApp/views.py
+++ StrechApp/views.py
@@ -1,7 +1,8 @@
 # from django.shortcuts import render
-import requests, json
+import requests
+import json
 from django.views.generic import ListView
-# from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from django.urls import reverse_lazy
 from django.shortcuts import render, redirect, get_object_or_404
@@ -10,7 +11,7 @@ from .models import exercises
 # from django.contrib.auth.models import User
 from users_app.models import CustomUser
 from django.contrib.auth.hashers import make_password
-from django.db.models import Q 
+from django.db.models import Q
 
 # Create your views here.
 # class Home(ListView):
@@ -22,24 +23,41 @@ def stretches(request):
     type = 'stretching'
     muscle = 'neck'
     api_url = f'https://api.api-ninjas.com/v1/exercises?muscle={muscle}&type={type}'
-    response = requests.get(api_url, headers={'X-Api-Key': '/eC7c8Yhpc3WiypBjxYKZg==bbWEvsg9V9HO5HKv'})
+    response = requests.get(
+        api_url, headers={'X-Api-Key': '/eC7c8Yhpc3WiypBjxYKZg==bbWEvsg9V9HO5HKv'})
     data = json.loads(response.text)
-    # q = request.GET.get"" 2hrs 6mns watch video 
-
+    q = request.GET.get('q')if request.GET.get('q') != None else ''
 
     muscles = exercises.objects.all()
+    # muscles = exercises.objects.filter(
+    #     Q(muscles__name__icontains = q)
+    #     )
 
     if response.status_code == requests.codes.ok:
         context = {
             "exc_list": data[0],
-            "exc_name":data[0]['name'],
-            "exc_equ":data[0]['equipment'],
-            "exc_inst":data[0]['instructions'],
-            'muscles':muscles
-
+            "exc_name": data[0]['name'],
+            "exc_equ": data[0]['equipment'],
+            "exc_inst": data[0]['instructions'],
+            'muscles': muscles
+            # graph context and calculate aray that holds demo, send to front end (graph js)
         }
-        # print('!!!!!!!!!!!!!!!!!!!!!!!!!', data[0]['name'])
-    else:
-        print("Error:", response.status_code, data)
-    
     return render(request, 'home.html', context)
+
+class add(CreateView, LoginRequiredMixin):
+    model = exercises
+    template_name = 'add.html'
+    fields = ['Bodypart', 'Duration', 'Person']
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+
+class edit(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = exercises
+    template_name = 'edit.html'
+    fields = ['Bodypart', 'Duration']
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.user
